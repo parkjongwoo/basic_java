94a763712fc9d419e92b01ffba2b3a7eb5665846
parkjongwoo <shoong1999@gmail.com>
2018-07-03 09:30:04 +0900

    2018.07.03
    충돌 제거

--- src/com/day07/oop/Ex04_sugar.java
+++ src/com/day07/oop/Ex04_sugar.java
@@ -5,14 +5,21 @@ import java.util.Scanner;
 public class Ex04_sugar {
 
 	public static void main(String[] args) {
-		SugarFactory sf = new SugarFactory();
 		Scanner sc = new Scanner(System.in);
-		System.out.println("설탕무게를 입력하세요.");
-		sf.setSugarStock(sc.nextInt());
 		
-		System.out.println(sf.getQuantity());
+		System.out.println("설탕무게를 입력하세요.");
+		System.out.println(getQuantity(sc.nextInt()));
 		
 		sc.close();
 	}
-
+	
+	static int getQuantity(int sugarStock) {
+		int ss = 0;//5kg봉지에 담을 총량을 제외한 남은 설탕량
+		for(int i=sugarStock/5;i>=0;i--) {//총량/5가 5kg봉지에 담을 수 있는 최고치,0은 5kg봉지가 없을 경우
+			ss = sugarStock - i * 5;//남은 량 산정
+			if(ss % 3 == 0) //남은 량이 3kg단위로 딱맞게 떨어지면
+				return i + ss / 3;//5kg봉지수 + 3kg봉지수 반환
+		}		
+		return -1;// 위에서 반환 안되면 소분 불가 -1반환
+	}
 }
